3edfaf003ebe4b1f10249e086ecfb20fc15d5077
zapboy216 <zapboy216@netzero.com>
2023-12-31 07:39:24 -0500

    Refactor cash register functionality and improve code readability

--- script.js
+++ script.js
@@ -1,76 +1,72 @@
-document.getElementById('purchase-btn').addEventListener('click', function() {
-    const price =15; // Example price, can be dynamic
-    const cash = parseFloat(document.getElementById('cash').value);
-    let cid = [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]; // Example cash in drawer
-    const changeDue = document.getElementById('change-due');
+// Prices and cash denominations
+const PRICE = 3.26;
+const CASH_IN_DRAWER = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
 
-    if (cash < price) {
-        alert('Customer does not have enough money to purchase the item');
-    } else {
-        let change = calculateChange(price, cash, cid);
-        changeDue.textContent = formatChangeOutput(change);
-    }
-});
+// DOM elements
+const displayChangeDueElement = document.getElementById('change-due');
+const cashInputElement = document.getElementById('cash');
+const purchaseButton = document.getElementById('purchase-btn');
+const priceScreenElement = document.getElementById('price-screen');
+const cashDrawerDisplayElement = document.getElementById('cash-drawer-display');
 
+// Format and display results
+const formatAndDisplayResults = (status, change) => {
+  displayChangeDueElement.innerHTML = `<p>Status: ${status}</p>`;
+  change.forEach(([denomination, amount]) => {
+    displayChangeDueElement.innerHTML += `<p>${denomination}: $${amount.toFixed(2)}</p>`;
+  });
+};
 
-function calculateChange(price, cash, cid) {
-    if (cash === price) {
-        return "No change due - customer paid with exact cash";
-    }
+// Check and process the cash register
+const processCashRegister = () => {
+  const cashAmount = parseFloat(cashInputElement.value);
+  if (isNaN(cashAmount)) {
+    alert('Invalid input. Please enter a numeric value.');
+    cashInputElement.value = '';
+    return;
+  }
 
-    let change = cash - price;
-    let totalCID = cid.reduce((sum, curr) => sum + curr[1], 0);
-    
-    if (totalCID < change) {
-        return " INSUFFICIENT_FUNDS";
-    }
+  if (cashAmount < PRICE) {
+    alert('Customer does not have enough money for the purchase.');
+    cashInputElement.value = '';
+    return;
+  }
 
-    let changeArray = cid.reverse().map(elem => {
-        let amount = 0;
-        const value = {
-            "ONE HUNDRED": 100.00,
-            "TWENTY": 20.00,
-            "TEN": 10.00,
-            "FIVE": 5.00,
-            "ONE": 1.00,
-            "QUARTER": 0.25,
-            "DIME": 0.10,
-            "NICKEL": 0.05,
-            "PENNY": 0.01
-        }[elem[0]];
+  if (cashAmount === PRICE) {
+    displayChangeDueElement.innerHTML = '<p>No change due - exact cash provided.</p>';
+    cashInputElement.value = '';
+    return;
+  }
 
-        while (change >= value && elem[1] > 0) {
-            amount += value;
-            elem[1] -= value;
-            change -= value;
-            change = Math.round(change * 100) / 100;
-        }
+  let changeDue = cashAmount - PRICE;
+  const reversedCashDrawer = [...CASH_IN_DRAWER].reverse();
+  const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  let result = { status: 'OPEN', change: [] };
 
-        return amount ? [elem[0], amount] : null;
-    }).filter(elem => elem != null);
+  let totalCashInDrawer = CASH_IN_DRAWER.reduce((total, [, amount]) => total + amount, 0);
 
-    if (change > 0) {
-        return "Status: INSUFFICIENT_FUNDS";
-    }
+  if (totalCashInDrawer < changeDue) {
+    formatAndDisplayResults('INSUFFICIENT_FUNDS', []);
+    return;
+  }
 
-    if (totalCID === cash - price) {
-        return "Status: CLOSED";
-    }
+  if (totalCashInDrawer === changeDue) {
+    formatAndDisplayResults('CLOSED', CASH_IN_DRAWER);
+    return;
+  }
 
-    return changeArray;
-}
-
-
-
-function formatChangeOutput(change) {
-    if (typeof change === 'string') {
-        return change;
-    }
-
-    return change.reduce((acc, curr) => {
-        acc += ` ${curr[0]}: $${curr[1].toFixed(2)}`;
-        return acc;
-    }, "Status: OPEN");
-}
-
-
+  reversedCashDrawer.forEach(([denomination, amount], index) => {
+    let count = 0;
+    while (amount > 0 && changeDue >= denominations[index]) {
+      amount -= denominations[index];
+      changeDue = parseFloat((changeDue - denominations
